014e9ea017cbb22f1b7ca47bf9a3aaf4d9e6e7bf
Rodney U <Rodneyu85@gmail.com>
2014-12-23 00:05:47 -0800

    Added support for editing.

--- app/src/main/java/qageek/com/simpletodo/MainActivity.java
+++ app/src/main/java/qageek/com/simpletodo/MainActivity.java
@@ -1,5 +1,6 @@
 package qageek.com.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,6 +10,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -21,6 +24,7 @@ public class MainActivity extends ActionBarActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    private final int REQUEST_CODE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,40 +35,6 @@ public class MainActivity extends ActionBarActivity {
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         setupListViewListener();
-    }
-
-    private void setupListViewListener() {
-        lvItems.setOnItemLongClickListener(
-            new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                    items.remove(position);
-                    itemsAdapter.notifyDataSetChanged();
-                    writeItems();
-                    return true;
-                }
-            }
-        );
-    }
-
-    private void readItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            items = new ArrayList<String>(FileUtils.readLines(todoFile));
-        } catch (IOException e) {
-            items = new ArrayList<String>();
-        }
-    }
-
-    private void writeItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            FileUtils.writeLines(todoFile, items);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -89,11 +59,79 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+            String newName = data.getExtras().getString("name");
+            int position = data.getExtras().getInt("position");
+            updateItem(position, newName);
+            Toast.makeText(this, newName, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView parent, View view, int position, long id) {
+                        items.remove(position);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+                }
+        );
+
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView parent, View view, int position, long id) {
+                        String text = ((TextView) view).getText().toString();
+                        launchEditItemView(position, text);
+                    }
+                }
+        );
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void onAddItem(View view) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
         writeItems();
+    }
+
+    public void updateItem(int position, String newName){
+        items.set(position, newName);
+        itemsAdapter.notifyDataSetChanged();
+        writeItems();
+    }
+
+    public void launchEditItemView(int position, String text){
+        Intent editIntent = new Intent(this, EditItemActivity.class);
+        editIntent.putExtra("text", text);
+        editIntent.putExtra("position", position);
+        startActivityForResult(editIntent, REQUEST_CODE);
     }
 }
